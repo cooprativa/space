5bb7492b81f9fab6b13bf8d56021cf5d616af8ad
Rafael Henriques <rafael.henriques99@gmail.com>
2026-03-03 13:11:05 +0000

    Add cursor hover showing names of the coaches in OurCoaches section.

--- src/components/sections/OurCoachesSection.tsx
+++ src/components/sections/OurCoachesSection.tsx
@@ -1,10 +1,24 @@
-import { forwardRef } from 'react';
+import { forwardRef, useState, useCallback } from 'react';
 import Coach1 from '../../assets/images/coach-1.png';
 import Coach2 from '../../assets/images/coach-2.png';
 import Coach3 from '../../assets/images/coach-3.png';
 import Coach4 from '../../assets/images/coach-4.png';
 
+const coaches = [
+  { src: Coach1, name: 'Tiago Lemos' },
+  { src: Coach2, name: 'João Dias' },
+  { src: Coach3, name: 'Marco Santos' },
+  { src: Coach4, name: 'Henrique Pereira' },
+];
+
 const OurCoachesSection = forwardRef<HTMLDivElement>((_, ref) => {
+  const [hoveredCoach, setHoveredCoach] = useState<string | null>(null);
+  const [cursorPos, setCursorPos] = useState({ x: 0, y: 0 });
+
+  const handleMouseMove = useCallback((e: React.MouseEvent) => {
+    setCursorPos({ x: e.clientX, y: e.clientY });
+  }, []);
+
   return (
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     <section className="section our-coaches" ref={ref as any}>
@@ -15,12 +29,29 @@ const OurCoachesSection = forwardRef<HTMLDivElement>((_, ref) => {
           </span>
         </h2>
       </div>
-      <div className="coaches">
-        <img src={Coach1} alt="Coach 1" />
-        <img src={Coach2} alt="Coach 2" />
-        <img src={Coach3} alt="Coach 3" />
-        <img src={Coach4} alt="Coach 4" />
+      <div
+        className="coaches"
+        onMouseMove={handleMouseMove}
+        onMouseLeave={() => setHoveredCoach(null)}
+      >
+        {coaches.map((coach, i) => (
+          <img
+            key={i}
+            className='faded-image'
+            src={coach.src}
+            alt={coach.name}
+            onMouseEnter={() => setHoveredCoach(coach.name)}
+          />
+        ))}
       </div>
+      {hoveredCoach && (
+        <div
+          className="coach-cursor-label"
+          style={{ left: cursorPos.x - 100, top: cursorPos.y + 20 }}
+        >
+          {hoveredCoach}
+        </div>
+      )}
     </section>
   );
 });
